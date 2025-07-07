0ab5385d6ca353877122893651fc0b79536bbc1e
narender <anymanagersdk@gmail.com>
2025-07-07 13:34:58 +0530

    Updating AnyManagerMediation Swift Package to 2.2.1

--- Package.swift
+++ Package.swift
@@ -1,46 +1,27 @@
 // swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// swift-tools-version: 6.0
 import PackageDescription
 
 // version 2.2.1
+
 let package = Package(
     name: "AnyManagerMediation",
     platforms: [.iOS(.v12)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AnyManagerMediation",
             targets: ["AnyManagerMediation"]),
     ],
     dependencies: [
-           .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.13.0"),
-       ],
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.13.0"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
+        .binaryTarget(
             name: "AnyManagerMediation",
-            dependencies: [
-                        .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
-                    ],
-            linkerSettings: [
-                            .linkedFramework("WebKit"),
-                            .linkedFramework("AdSupport"),
-                            .linkedFramework("AVFoundation"),
-                            .linkedFramework("CoreGraphics"),
-                            .linkedFramework("CoreMedia"),
-                            .linkedFramework("CoreTelephony"),
-                            .linkedFramework("StoreKit"),
-                            .linkedFramework("SystemConfiguration"),
-                            .linkedFramework("UIKit"),
-                            .linkedFramework("AudioToolbox"),
-                            .linkedFramework("CFNetwork"),
-                            .linkedFramework("MediaPlayer"),
-                            .linkedFramework("QuartzCore"),
-                            
-            ]
-        
+            url: "https://github.com/AnyMindG/AnyManagerMediation/releases/download/2.2.1/AnyManagerMediation.zip",
+            checksum: "887058455e2324c317ece4534e1e8e0452481621b86e3d860787a03af30ec815"
         ),
         .testTarget(
             name: "AnyManagerMediationTests",
@@ -48,3 +29,4 @@ let package = Package(
         ),
     ]
 )
+
